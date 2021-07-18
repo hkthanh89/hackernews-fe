6c79db98f4d6202b1097e00fc6e2076ab4b721e6
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-18 13:24:12 +0700

    add news detail page

---
+++ pages/news/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useRouter } from 'next/router'
+import useSWR from 'swr';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Loading from '../../src/Loading';
+
+import fetcher from '../../lib/fetcher';
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    width: "100%",
+    height: "auto",
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  buttonContainer: {
+    textAlign: "center",
+    marginBottom: theme.spacing(4),
+  }
+}));
+
+export default function News() {
+  const classes = useStyles()
+  const router = useRouter()
+  const { url } = router.query
+
+  const { data, error } = useSWR(url ? `http://localhost:3000/news?url=${url}` : null, fetcher)
+
+  const isLoading = !data;
+
+  const news = !isLoading ? data.data : {}
+
+  console.log('data', data)
+  console.log('error', error)
+
+  console.log(news)
+
+  return (
+    <React.Fragment>
+      <main>
+        <Loading isLoading={isLoading} />
+        <Container maxWidth="md">
+          <img src={news.cover_image_url} alt={news.title} className={classes.image} />
+          <Typography gutterBottom variant="h4" component="h2" align="center">
+            {news.title}
+          </Typography>
+          <div dangerouslySetInnerHTML={{ __html: news.content }} />
+        </Container>
+        <Container maxWidth="md" className={classes.buttonContainer}>
+          <Button variant="outlined" color="primary">
+            <Link href="/" underline="none">
+              Back
+            </Link>
+          </Button>
+        </Container>
+      </main>
+    </React.Fragment>
+  )
+}
