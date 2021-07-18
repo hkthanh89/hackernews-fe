6a78fd3be651576fe2d87f8916ab1401a15ddbba
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-18 13:23:43 +0700

    change primary color

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { red } from '@material-ui/core/colors';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#10B981',
     },
     secondary: {
       main: '#19857b',
