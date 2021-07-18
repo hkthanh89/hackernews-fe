90801c1acdd4b6e96019c65b3e117d10fba3832c
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-18 12:24:26 +0700

    redirect to root page when clicking header

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 export default function Header() {
   return (
     <React.Fragment>
       <AppBar position="relative">
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Best Hacker News
-          </Typography>
+          <Link href="/" underline="none">
+            <Typography variant="h6" noWrap style={{color: '#fff'}}>
+              Best Hacker News
+            </Typography>
+          </Link>
         </Toolbar>
       </AppBar>
     </React.Fragment>
