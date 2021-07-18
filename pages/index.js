a40a2e8793be08c1f725e05dd91278fde2e54f7f
Thanh Huynh <hkthanh89@users.noreply.github.com>
2021-07-18 12:11:07 +0700

    add shared Header

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,21 @@
 import React, { useState } from 'react';
-import useSWR from 'swr'
-import AppBar from '@material-ui/core/AppBar';
+import useSWR from 'swr';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Copyright from '../src/Copyright';
-import fetch from 'unfetch';
+import Loading from '../src/Loading';
+import fetcher from '../lib/fetcher';
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  loading: {
-    textAlign: 'center',
-    margin: theme.spacing(2)
-  },
   heroButtons: {
     marginTop: theme.spacing(1),
   },
@@ -53,8 +43,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const fetcher = url => fetch(url).then(r => r.json())
-
 export default function ListNews() {
   const classes = useStyles();
 
@@ -71,45 +59,32 @@ export default function ListNews() {
 
   return (
     <React.Fragment>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Best Hacker News
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <main>
         {error && <div>Failed to load</div>}
-        {isLoading && <div className={classes.loading}><CircularProgress /></div>}
+        <Loading isLoading={isLoading} />
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
             {listNews.map((news) => (
               <Grid item key={news.url} xs={12} sm={6} md={3}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={news.cover_image_url}
-                    isMediaComponent
-                    src={news.cover_image_url}
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="body1" component="h2" className={classes.title}>
-                      {news.title}
-                    </Typography>
-                    <Typography gutterBottom variant="caption" component="h2" align="right">
-                      {news.sub_title}
-                    </Typography>
-                    <Typography variant="body2" component="h2">
-                      {news.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                  </CardActions>
-                </Card>
+                <Link href={`/news?url=${news.url}`} underline="none">
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={news.cover_image_url}
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="body1" component="h2" className={classes.title}>
+                        {news.title}
+                      </Typography>
+                      <Typography gutterBottom variant="caption" component="h2" align="right">
+                        {news.sub_title}
+                      </Typography>
+                      <Typography variant="body2" component="h2">
+                        {news.description}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Link>
               </Grid>
             ))}
           </Grid>
